a762f459be9430850312e3537eca6c75965ab584
Joshua Offer <joshuaoffer@hotmail.co.uk>
2015-04-08 10:28:04 +0100

    Created home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about" # creates an about_path (You must use this exact word when creating links from home page)
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
