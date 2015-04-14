910da1386dd953a74d7fa2ea2158643b95cbb253
Joshua Offer <joshuaoffer@hotmail.co.uk>
2015-04-14 14:39:46 +0100

    Added user logins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "pages#home"
   get "about" => "pages#about" # creates an about_path (You must use this exact word when creating links from home page)
   get "test" => "pages#test"
